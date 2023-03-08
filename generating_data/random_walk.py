505905deee94c595b1b22cef0bc4f507626b1bb1
Lucas Garzuze Cordeiro <118022446+lucas-garzuze-cordeiro@users.noreply.github.com>
2023-03-08 18:54:15 -0300

    Refatorando módulo
    Deixei o método fill_walk() menos longo. Assim deixando a classe mais fácil de entender.

--- generating_data/random_walk.py
+++ generating_data/random_walk.py
@@ -6,36 +6,43 @@
 from random import choice
 
 class RandomWalk:
-    """Uma classe que gera caminhos aleatórios"""
+    """Uma classe que gera pontos aleatórios num gráfico"""
 
     def __init__(self, num_points=5000):
-        """Inicializa os atributos da caminhada"""
+        """Inicializa os atributos do mapa de pontos"""
         self.num_points= num_points
 
         # Todos os caminhos começam em (0, 0)
         self.x_values = [0]
         self.y_values = [0]
 
+    def get_step(self):
+        """Determina a direção e a distância de cada ponto"""
+
+        # Decide em qual direção ir e quão longe ir
+        x_direction = choice([-1, 1])
+        x_distance = choice([0, 1, 2, 3, 4])
+        x_step = x_direction * x_distance
+
+        y_direction = choice([-1, 1])
+        y_distance = choice([0, 1, 2, 3, 4])
+        y_step = y_direction * y_distance
+
+        return (x_step, y_step)
+    
     def fill_walk(self):
-        """Calcula todos os pontos no caminho"""
+        """Preenche o gráfico com vários pontos"""
 
-        # Continue andando até que a caminhada atinga o tamanho desejado
+        # Continue gerando passos até que a caminhada atinga o tamanho desejado
         while len(self.x_values) < self.num_points:
-
-            # Decide em qual direção ir e quão longe ir
-            x_direction = choice([-1, 1])
-            x_distance = choice([0, 1, 2, 3, 4])
-            x_step = x_direction * x_distance
-
-            y_direction = choice([-1, 1])
-            y_distance = choice([0, 1, 2, 3, 4])
-            y_step = y_direction * y_distance
+            # Calcula a posição do ponto
+            x_step = self.get_step()[0]
+            y_step = self.get_step()[1]
 
             # Rejeita movimentos que não vão a lugar nenhum
             if x_step == 0 and y_step == 0:
                 continue
-            
-            # Calcula a nova posição
+
             x = self.x_values[-1] + x_step
             y = self.y_values[-1] + y_step
 
