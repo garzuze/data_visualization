5f1611b9f8bf6213a61468864801181af56df633
Lucas Garzuze Cordeiro <118022446+lucas-garzuze-cordeiro@users.noreply.github.com>
2023-03-12 19:48:16 -0300

    Visualização dos dados obtidos
    Depois de jogar um dado 1000 vezes, eu peguei os dados do resultado e fiz um gráfico de barras comparando os números que saíram.
    É muito legal fazer uma visualização de dados de um dado porque você pode ficar brincando com os signifidados da palavra dado.
    Dado esse informação, sinta-se a vontade para dar uma olhada nos meus dados sobre os resuldados obtidos a partir dos dados de jogar um dado 1000 vezes.
    Tá bom, parei.

--- generating_data/die_visual.py
+++ generating_data/die_visual.py
@@ -1,6 +1,9 @@
 # 08/03/2023 - Lucas Garzuze Cordeiro
 # Simular os resultados de um dado com 6 lados sendo jogado
 
+from plotly.graph_objs import Bar, Layout
+from plotly import offline
+
 from die import Die
 
 # Criando um D6
@@ -13,4 +16,21 @@ for roll_num in range(1000):
     result = die.roll()
     results.append(result)
 
-print(results)
+# Analisando os resultados
+frequencies = []
+
+for value in range(1, die.num_sides+1):
+    frequencies.append(results.count(value))
+
+# Visualizando os resultados
+
+x_values = list(range(1, die.num_sides+1))
+data = [Bar(x=x_values, y=frequencies)]
+
+x_axis_config = {'title': 'Resultado'}
+y_axis_config = {'title': 'Frequencia dos resultados'}
+
+my_layout = Layout(title='Resultados de jogar o dado 1000 vezes',
+                   xaxis=x_axis_config, yaxis=y_axis_config)
+
+offline.plot({'data': data, 'layout': my_layout}, filename='d6.html')
