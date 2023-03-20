0d9947441e1912e83ad474df976e5a29a43905c6
Lucas Garzuze Cordeiro <118022446+lucas-garzuze-cordeiro@users.noreply.github.com>
2023-03-20 14:18:35 -0300

    Ajustando alguns erros de lógica
    Antes o código só mostrava a frequência dos resultados até o 12. Mas como eu estou multiplicando dois D6s, o resultado máximo deve ser 36. Agora está funcionando como deveria.

--- generating_data/multiplication.py
+++ generating_data/multiplication.py
@@ -13,23 +13,14 @@ die_1 = Die()
 die_2 = Die()
 
 # Jogar os dados algumas vezes e armazenar numa lista
-results = []
-
-for roll_num in range(50_000):
-    result = die_1.roll() * die_2.roll()
-    results.append(result)
+results = [die_1.roll() * die_2.roll() for roll_num in range(50_000)]
 
 # Analisando os resultados
-frequencies = []
-max_result = die_1.num_sides + die_2.num_sides
-
-# (a lista começa no três pois é o menor resultado possível rolando 3 dados)
-for value in range(2, max_result+1):
-    frequencies.append(results.count(value))
+max_result = die_1.num_sides * die_2.num_sides
+frequencies = [results.count(value) for value in range(1, max_result+1)]
 
 # Visualizando os resultados
-
-x_values = list(range(2, max_result+1))
+x_values = list(range(1, max_result+1))
 data = [Bar(x=x_values, y=frequencies)]
 
 x_axis_config = {'title': 'Resultado', 'dtick': 1}
