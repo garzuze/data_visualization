2b4f37d2be330171d35caad229cb41f4cd76ab24
Lucas Garzuze Cordeiro <118022446+lucas-garzuze-cordeiro@users.noreply.github.com>
2023-04-03 18:38:23 -0300

    Achando os indexes mais facilmente
    No código anterior era necessário procurar os indexes printando a linha de cabeçalho e achando manualmente. Agora isso não é mais necessário. Dá para fazer isso com os outros códigos de análise de temperatura.

--- downloading_data/sitka_highs_and_lows.py
+++ downloading_data/sitka_highs_and_lows.py
@@ -16,21 +16,22 @@ filename = r'data/sitka_weather_2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    print(header_row)
+    max_temperature = header_row.index("TMAX")
+    min_temperature = header_row.index("TMIN")
 
     # Pegar as datas e as temperaturas mais altas desse arquivo
     dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[2], '%Y-%m-%d')
-        high = convert_to_celsius(int(row[5]))
-        low = convert_to_celsius(int(row[6]))
+        high = convert_to_celsius(int(row[max_temperature]))
+        low = convert_to_celsius(int(row[min_temperature]))
         dates.append(current_date)
         highs.append(high)
         lows.append(low)
 
 # Gráfico com as temperaturas mais altas
 
-plt.style.use('seaborn')
+plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
 ax.plot(dates, highs, c='lightcoral')
 ax.plot(dates, lows, c='lightskyblue')
@@ -39,7 +40,7 @@ ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # Formatando o gráfico
 
-ax.set_title('Temperaturas diárias mais altas e baixas - Sitka 2018', fontsize=20)
+ax.set_title(f'Temperaturas diárias mais altas e baixas - Sitka, 2018', fontsize=20)
 ax.set_xlabel("", fontsize=12)
 fig.autofmt_xdate()
 ax.set_ylabel("Temperatura (C°)", fontsize=12)
