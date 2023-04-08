b3dfc2e29a8b8cfe3ebe7fdd7356668f6d9015e7
Lucas Garzuze Cordeiro <118022446+lucas-garzuze-cordeiro@users.noreply.github.com>
2023-04-08 10:25:54 -0300

    Um mapa com terremotos ocorridos
    Como capítulo do livro, fiz um gráfico com o mapa mundial contendo todas as ocorrências de terremotos num período de 30 dias. Aprendemos a extrair informações de arquivos .json, a fazer gráficos com mapas, a editar marcadores em seus tamanhos e escalas de cores.  Impressionante como podemos fazer um gráfico tão completo em umas 50 linhas de código.

--- downloading_data/earthquakes_visualizations/eq_explore_data.py
+++ downloading_data/earthquakes_visualizations/eq_explore_data.py
@@ -4,9 +4,10 @@ import json
 from plotly.graph_objs import Scattergeo, Layout
 from plotly import offline
 
-# Deixar o arquivo .json legívelp
+# Deixar o arquivo .json legível
 # eq = abreviação de earthquake (terremoto)
 
+# Dados de terremotos ocorridos nos últimos 30 dias
 filename = 'data/eq_data_30_day_m1.json'
 
 with open(filename) as f:
@@ -17,33 +18,38 @@ readable_file = 'data/readable_eq_data.json'
 with open(readable_file, 'w') as f:
     json.dump(all_eq_data, f, indent=4)
 
-# Extraindo todos os dicionários e verificando quantos dicionários existem
+# Extraindo todos os dicionários
 all_eq_dicts = all_eq_data['features']
-print(len(all_eq_dicts))
 
-# Extraindo magnitudes, longitudes e latitudes
-mags, lons, lats = [], [], []
+# Extraindo magnitudes, longitudes, latitudes e títulos para cada ponto
+mags, lons, lats, hover_texts = [], [], [], []
 
 for eq_dict in all_eq_dicts:
     mag = eq_dict['properties']['mag']          # magnitude
     lon = eq_dict['geometry']['coordinates'][0] # longitude
     lat = eq_dict['geometry']['coordinates'][1] # latitude
+    title = eq_dict['properties']['title']      # título do terremoto
     mags.append(mag)
     lons.append(lon)
     lats.append(lat)
+    hover_texts.append(title)
 
 # Mapeando os terremotos
-
 data = [{
     'type': 'scattergeo',
     'lon': lons,
     'lat': lats,
+    'text': hover_texts,
     'marker': {
         'size': [4*mag for mag in mags],
+        'color': mags,
+        'colorscale': 'Hot',
+        'reversescale': True,
+        'colorbar': {'title': 'Magnitude'}
     }
      }]
 
-my_layout = Layout(title='Terremotos globais')
+my_layout = Layout(title='Terremotos mundiais')
 
 fig = {'data': data, 'layout': my_layout}
 offline.plot(fig, filename="terremotos_mundiais.html")
